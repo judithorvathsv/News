c92c896b78c5a56e3eaa540382dd1b3cbc2114dd
Judit Horvath <judit.horvath@appliedtechnology.se>
2024-11-30 11:16:45 +0100

    Add article fullscreen and clean code

--- javascript.js
+++ javascript.js
@@ -1,62 +1,82 @@
-function toggleHiddenNavbar(){
+function toggleHiddenNavbar() {
     const topicList = document.querySelector('.header__nav-topic-dropdown');
-    topicList.classList.toggle("showned-element");   
+    topicList.classList.toggle("showned-element");
 }
 
-function toggleHiddenLogin(){
+function toggleHiddenLogin() {
     const loginForm = document.querySelector('.header__login');
-    loginForm.classList.toggle("showned-element");   
+    loginForm.classList.toggle("showned-element");
 
     const registerForm = document.querySelector('.header__register');
-    registerForm.classList.remove("showned-element"); 
+    registerForm.classList.remove("showned-element");
 }
 
-function toggleHiddenRegister(){
+function toggleHiddenRegister() {
     const registerForm = document.querySelector('.header__register');
-    registerForm.classList.toggle("showned-element");  
-    
+    registerForm.classList.toggle("showned-element");
+
     const loginForm = document.querySelector('.header__login');
-    loginForm.classList.remove("showned-element"); 
+    loginForm.classList.remove("showned-element");
 }
 
 function updateClampClassMobileShort() {
-    const articleTexts = document.querySelectorAll('.main__article-text-mobile-short'); 
+    const articleTexts = document.querySelectorAll('.main__article-text-mobile-short');
     const width = window.innerWidth;
 
-    articleTexts.forEach(articleText => {     
+    articleTexts.forEach(articleText => {
         articleText.classList.remove('clamp-2', 'clamp-6');
 
-        if (width < 768) { 
+        if (width < 768) {
             articleText.classList.add('clamp-2');
-        } else if (width >= 768 ) { 
+        } else if (width >= 768) {
             articleText.classList.add('clamp-6');
-        }     
+        }
     });
 }
-// Initial check
+
 updateClampClassMobileShort();
-// Update on resize
 window.addEventListener('resize', updateClampClassMobileShort);
 
 
-function updateClampClassTabletLong() {   
-    const articleTexts = document.querySelectorAll('.main__article-text-tablet-long');    
+function updateClampClassTabletLong() {
+    const articleTexts = document.querySelectorAll('.main__article-text-tablet-long');
     const width = window.innerWidth;
 
-    articleTexts.forEach(articleText => {    
+    articleTexts.forEach(articleText => {
         articleText.classList.remove('clamp-2', 'clamp-6');
 
-        if (width < 768) { 
+        if (width < 768) {
             articleText.classList.add('clamp-2');
-        } else if (width >= 768 && width < 1024) { 
+        } else if (width >= 768 && width < 1024) {
             articleText.classList.add('clamp-6');
-        } else { 
+        } else {
             articleText.classList.add('clamp-2');
         }
     });
 }
-// Initial check
+
 updateClampClassTabletLong();
-// Update on resize
 window.addEventListener('resize', updateClampClassTabletLong);
+
+
+
+function toggleFullscreen(article) {
+    if (article.classList.contains('fullscreen')) {
+        // Exit fullscreen
+        article.classList.remove('fullscreen');
+        const articleText = article.querySelector('.main__article-text');
+        const image = article.querySelector('.main__article-image');
+        image.style.display = ''; // Reset to default display
+    } else {
+        // Enter fullscreen
+        article.classList.add('fullscreen');
+        const image = article.querySelector('.main__article-image');
+        image.style.display = 'none'; // Hide image in fullscreen
+    }
+}
+
+var articles = document.querySelectorAll('.main-article');
+for (var i = 0; i < articles.length; i++) {
+    articles[i].setAttribute('onclick', 'toggleFullscreen(this)');
+}
 
